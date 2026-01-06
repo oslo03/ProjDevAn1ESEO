368abac73e06f2398c0c4bbe8a5790a1b29ef417
ndauga <nathan.dauga03@gmail.com>
2026-01-06 08:33:52 +0100

    Base 06012026

--- Class/AnalyseurTrajets.py
+++ Class/AnalyseurTrajets.py
@@ -3,7 +3,6 @@ import TrajetObserve as trjObs
 #Classe de trajet observée, avec la liste des stations parcourus et la meusure de temps et de distance.
 
 class AnalyseurTrajets:
-
     # Classe contenant differante méthode pour analyser les différents trajet..
     # Contient TODO
     def __init__(self, reseau, trajObserve):
